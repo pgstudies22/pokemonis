4d8622fb10badc603f4aac064279cfc28e9cdaa7
pgstudies22 <107188587+pgstudies22@users.noreply.github.com>
2023-04-03 22:49:49 -0300

    Informações de paginação isoladas

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const getTypeColor = type => ({
-  steel: '#f4f4f4',
-  fire: '#FDDFDF',
-  grass: '#DEFDE0',
-  electric: '#FCF7DE',
-  ice: '#DEF3FD',
-  water: '#DEF3FD',
-  ground: '#f4e7da',
-  rock: '#d5d5d4',
-  fairy: '#fceaff',
-  poison: '#98d7a5',
-  bug: '#f8d5a3',
-  ghost: '#cac0f7',
-  dragon: '#97b3e6',
-  psychic: '#eaeda1',
-  flying: '#F5F5F5',
-  fighting: '#E6E0D4',
-  normal: '#F5F5F5'
-})[type] || '#F5F5F5'
+const getTypeColor = type => {
+  const normal = '#F5F5F5'
+  return {
+    normal,
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    ice: '#DEF3FD',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    ghost: '#cac0f7',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    fighting: '#E6E0D4'
+  }[type] || normal
+}
 
 const renderPokemons = pokemons => {
   const ul = document.querySelector('ul')
@@ -47,20 +48,44 @@ const renderPokemons = pokemons => {
 const getOnlyFulfilled = async ({ arr, func }) => {
   const promises = arr.map(func)
   const responses = await Promise.allSettled(promises)
-  return responses.filter(r => r.status === 'fulfilled')
+  return responses.filter(response => response.status === 'fulfilled')
 }
+
+const getPokemonsType = async pokeApiResults => {
+  const fulfilledUrls = await getOnlyFulfilled({ arr: pokeApiResults, func: result => fetch(result.url) })
+  const promisesUrls = fulfilledUrls.map(t => t.value.json())
+  const resolvedFulfilledResults = await Promise.all(promisesUrls)
+  return resolvedFulfilledResults.map(resolved => resolved.types.map(t => t.type.name))
+}
+
+const getPokemonsImgs = async ids => {
+  const fulfilledImgs = await getOnlyFulfilled({ arr: ids, func: id => fetch(`./assets/img/${id}.png`) })
+  return fulfilledImgs.map(t => t.value.url)
+}
+
+const getPokemonsIds = pokeApiResults => pokeApiResults
+  .map(({ url }) => url.split('/')[url.split('/').length - 2])
 
 const observeLastPokemon = pokemonsObserver => {
   const lastPokemon = document.querySelector('ul').lastChild
   pokemonsObserver.observe(lastPokemon)
 }
 
-const limit = 15
-let offset = 0
+const paginationInfo = (() => {
+  const limit = 15
+  let offset = 0
+
+  const getOffset = () => offset
+  const getLimit = () => limit
+  const incrementOffset = () => offset += limit
+  
+  return { getOffset, getLimit, incrementOffset }
+})()
 
 const getPokemons = async () => {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    const { getOffset, getLimit, incrementOffset } = paginationInfo
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${getOffset()}&limit=${getLimit()}`)
 
     if (!response.ok) {
       throw new Error('Não foi possível obter as informações')
@@ -71,8 +96,8 @@ const getPokemons = async () => {
     const ids = getPokemonsIds(pokeApiResults)
     const imgUrls = await getPokemonsImgs(ids)
     const pokemons = ids.map((id, i) => ({ id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgUrls[i] }))
-    offset += limit
 
+    incrementOffset()
     return pokemons
   } catch (error) {
     console.log('Algo deu errado:', error)
@@ -87,7 +112,7 @@ const handleNextPokemonsRender = () => {
 
     observer.unobserve(lastPokemon.target)
 
-    if (offset >= 150) {
+    if (paginationInfo.getOffset() >= 150) {
       return
     }
     
@@ -100,26 +125,9 @@ const handleNextPokemonsRender = () => {
   observeLastPokemon(pokemonsObserver)
 }
 
-const getPokemonsType = async pokeApiResults => {
-  const resultTypes = pokeApiResults.map(result => fetch(result.url))
-  const settledResultTypes = await Promise.allSettled(resultTypes)
-  const fulfilledResults = settledResultTypes.filter(q => q.status === 'fulfilled').map(t => t.value.json())
-  const resolvedFulfilledResults = await Promise.all(fulfilledResults)
-  return resolvedFulfilledResults.map(resolved => resolved.types.map(t => t.type.name))
-}
-
-const getPokemonsImgs = async ids => {
-  const imgResponses = ids.map(id => fetch(`./assets/img/${id}.png`))
-  const imgPromises = await Promise.allSettled(imgResponses)
-  return imgPromises.filter(q => q.status === 'fulfilled').map(t => t.value.url)
-}
-
-const getPokemonsIds = pokeApiResults => pokeApiResults
-  .map(({ url }) => url.split('/')[url.split('/').length - 2])
-
 const handlePageLoad = async () => {
   const pokemons = await getPokemons()
-
+  
   renderPokemons(pokemons)
   handleNextPokemonsRender()
 }
